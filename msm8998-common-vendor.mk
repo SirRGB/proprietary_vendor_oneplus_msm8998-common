34f9ca925fe4f7f89a1ad172a4efc617b1f46166
SirRGB <sirrgb@proton.me>
2025-03-17 19:35:29 +0100

    msm8998-common: fix com.qualcomm.qti.dpm.api@1.0 ELF checks
    Change-Id: If63b4b04d82c8999affe23bd72f7855fa2ff1024

--- msm8998-common-vendor.mk
+++ msm8998-common-vendor.mk
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
     libvqzip \
     libasphere \
     btaudio_offload_if \
-    com.qualcomm.qti.dpm.api@1.0 \
+    com.qualcomm.qti.dpm.api@1.0_vendor \
     com.qualcomm.qti.imscmservice@2.0 \
     com.qualcomm.qti.imscmservice@2.1 \
     com.qualcomm.qti.imscmservice@2.2 \
